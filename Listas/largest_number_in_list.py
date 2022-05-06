3f0b1035524ac0468b0da857141120047593812c
i5s2ac <103969157+i5s2ac@users.noreply.github.com>
2022-05-05 19:00:36 -0600

    Algorithm No. 1: Largest Number in a List

---
+++ Listas/largest_number_in_list.py
@@ -0,0 +1,10 @@
+def find_max(arr):
+
+  max_number = arr[0]
+
+  for i in range(0, len(arr)):
+    if arr[i] > max_number:
+      max_number = arr[i]
+
+  return max_number
+  
---
+++ testing.py
@@ -0,0 +1,11 @@
+import Listas
+from Listas.largest_number_in_list import find_max
+
+print("----------------------------| ALGORITHM # 1: LARGEST NUMBER IN A LIST |----------------------------  ")
+print("")
+
+arr=[2, 5, 1, 7, 10]
+find_max(arr)
+print("El número más grande en la lista es: ",find_max(arr))
+
+print("")
